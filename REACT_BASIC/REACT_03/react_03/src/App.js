b72f38050a4ce98787cf5cd869cb0d8a762fbfb0
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-03-08 08:31:23 +0100

    Added an another component

--- REACT_BASIC/REACT_03/react_03/src/App.js
+++ REACT_BASIC/REACT_03/react_03/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'; //Import another Component to add to main Component (App.js)
 
 class App extends Component {
   render() {
-  // return (
-  //   <div className="App">
-  //     <h>Hi</h>
-  //   </div>
-  // );
-  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'What is new?')); //React.createElement(component, props, ...children)
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <p>This is really working!</p>
+      <Person /> 
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'What is new?')); //React.createElement(component, props, ...children)
   }
 }
 
