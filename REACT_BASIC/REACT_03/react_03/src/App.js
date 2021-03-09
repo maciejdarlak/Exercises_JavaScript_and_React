13e54c3285f5b8262799fa6b002c9045344beefa
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-03-09 08:34:56 +0100

    Added state (js object) as an array.

--- REACT_BASIC/REACT_03/react_03/src/App.js
+++ REACT_BASIC/REACT_03/react_03/src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import Person from './Person/Person'; //Import another Component to add to main Component (App.js)
 
 class App extends Component {
+  //State is an internal component part.
+  state = { 
+    persons: [
+      { name: 'Max', age: 40 },
+      { name: 'Angie', age: 20 },
+      { name: 'James', age: 25 }
+    ]
+  }
+
   render() {
   return (
     <div className="App">
       <h1>Hi</h1>
       <p>This is really working!</p>
-      <Person /> 
-      <Person />
-      <Person />
-      <Person />
+      <button>Switch name</button> 
+      <Person  name={this.state.persons[0].name} age={this.state.persons[0].age} > I have a dog! </Person>
+      <Person  name={this.state.persons[1].name} age={this.state.persons[1].age} > I have a hamster! </Person>
+      <Person  name={this.state.persons[2].name} age={this.state.persons[2].age} /> 
     </div>
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'What is new?')); //React.createElement(component, props, ...children)
   }
 }
 
-export default App;
+export default App; 
--- REACT_BASIC/REACT_03/react_03/src/Person/Person.js
+++ REACT_BASIC/REACT_03/react_03/src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old!</p> //Math.floor - rounding
-}
+const person = (props) => {
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old!</p> 
+            <p>{props.childern}</p>
+        </div>
+    )
+};
 
 export default person;
