c776c54583d455d2c48053cb92c5f73e25662951
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-05-11 08:49:13 +0200

    Extracting the ExpenseDate component from ExpenseItem + adding individual css files

--- REACT_BASIC/REACT_05/react_05/src/components/ExpenseItem.js
+++ REACT_BASIC/REACT_05/react_05/src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
 
 //Props which are used in 'App' file
 function ExpenseItem(props) {
+
     return (
-        <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+        <div className='expense-item'>  
+        <ExpenseDate />        
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
