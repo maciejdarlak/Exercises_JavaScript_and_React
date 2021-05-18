975bc38c2eea42874eb4db246ccb0f48496a5c6b
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-05-18 08:56:08 +0200

    Adding state to file

--- REACT_BASIC/REACT_05/react_05/src/components/Expenses/ExpenseItem.js
+++ REACT_BASIC/REACT_05/react_05/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react'; // event: use 'useState'
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title); // event: the rule is: [present value, value after 'event']
+
+  const clickHandler = () => { 
+    setTitle('Updated!'); // event: value after 'event'
+    console.log(title); // event: calling 'title' after change
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-      </div>
+      </div> 
+      <button onClick={clickHandler}>Change Title</button> {/* event: calling 'clickHandler'*/}
     </Card>
   );
 }
