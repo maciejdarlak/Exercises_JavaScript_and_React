ee66da3d192cad2b41f74ad01e88100485212977
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-07-12 15:19:48 +0200

    Added a new state exercise

--- REACT_06/react_06/src/App.js
+++ REACT_06/react_06/src/App.js
@@ -4,93 +4,93 @@ import React from 'react';
 
 
 
-// class AppHeader extends React.Component {
-//   render() {
-//     return (
-//       <header className='ui fixed menu'>
-//         <nav className='ui container'>
-//           <a href='#' className='header item'>
-//             <img
-//               className='logo'
-//               src='https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png'
-//             />
-//             Lista kontaktów
-//           </a>
-//           <div className='header item'>
-//             <button className='ui button'>Dodaj</button>
-//           </div>
-//         </nav>
-//       </header>
-//     );
-//   }
-// }
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <header className='ui fixed menu'>
+        <nav className='ui container'>
+          <a href='#' className='header item'>
+            <img
+              className='logo'
+              src='https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png'
+            />
+            Lista kontaktów
+          </a>
+          <div className='header item'>
+            <button className='ui button'>Dodaj</button>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
 
-// class ContactList extends React.Component {
-//   render() {
-//     return(
-//       <ul className='ui relaxed divided list selection'>
-//         <ContactItem
-//           login='typeofweb1'
-//           name='Lena'
-//           department='JavaScript Developer'
-//         />
-//         <ContactItem
-//           login='typeofweb2'
-//           name='Brian'
-//           department='Human Resources'
-//         />
-//         <ContactItem
-//           login='typeofweb3'
-//           name='Rick'
-//           department='QA'
-//         />
-//       </ul>
-//     );
-//   }
-// }
+class ContactList extends React.Component {
+  render() {
+    return(
+      <ul className='ui relaxed divided list selection'>
+        <ContactItem
+          login='typeofweb1'
+          name='Lena'
+          department='JavaScript Developer'
+        />
+        <ContactItem
+          login='typeofweb2'
+          name='Brian'
+          department='Human Resources'
+        />
+        <ContactItem
+          login='typeofweb3'
+          name='Rick'
+          department='QA'
+        />
+      </ul>
+    );
+  }
+}
 
 
-// class ContactItem extends React.Component {
-//   render() {
-//     const { login, name, department } = this.props
-//     const imgUrl = 'https://api.hello-avatar.com/adorables/55/' + {login} + '.png';
-//     return (
-//       <li className="item">
-//         <img src={imgUrl} className="ui mini rounded image" />
-//         <div className="content">
-//         <NewButtons />
-//           <h4 className="header">{name}</h4>
-//           <div className="description">{department}</div>
-//         </div>
-//       </li>
-//     );
-//   }
-// }
+class ContactItem extends React.Component {
+  render() {
+    const { login, name, department } = this.props
+    const imgUrl = 'https://api.hello-avatar.com/adorables/55/' + {login} + '.png';
+    return (
+      <li className="item">
+        <img src={imgUrl} className="ui mini rounded image" />
+        <div className="content">
+        <NewButtons />
+          <h4 className="header">{name}</h4>
+          <div className="description">{department}</div>
+        </div>
+      </li>
+    );
+  }
+}
 
 
-// class NewButtons extends React.Component {
-//   render() {
-//     return <button onClick={this.onClickHandler}>Kliknij!</button>;
-//   }
-//   onClickHandler() {
-//     alert("Kliknięto!");
-//   }
-// }
+class NewButtons extends React.Component {
+  render() {
+    return <button onClick={this.onClickHandler}>Kliknij!</button>;
+  }
+  onClickHandler() {
+    alert("Kliknięto!");
+  }
+}
 
 
-// function App() { 
-//   return (
-//       <div>
-//         <AppHeader />
-//         <main className='ui main text container'>
-//           <ContactList />
-//         </main>
-//       </div>
-//     );
-//   }
+function App() { 
+  return (
+      <div>
+        <AppHeader />
+        <main className='ui main text container'>
+          <ContactList />
+        </main>
+      </div>
+    );
+  }
 
-// export default App;
+export default App;
 
 
 
@@ -101,92 +101,92 @@ import React from 'react';
 
 // Below functions instead of classes + use of different avatars depending on whether the login is an e-mail or not
 
-// function AppHeader() {
-//   return (
-//     <header className='ui fixed menu'>
-//       <nav className='ui container'>
-//         <a href='#' className='header item'>
-//           <img
-//             className='logo'
-//             src='https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png'
-//           />
-//           Lista kontaktów
-//         </a>
-//         <div className='header item'>
-//           <button className='ui button'>Dodaj</button>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// }
+function AppHeader() {
+  return (
+    <header className='ui fixed menu'>
+      <nav className='ui container'>
+        <a href='#' className='header item'>
+          <img
+            className='logo'
+            src='https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png'
+          />
+          Lista kontaktów
+        </a>
+        <div className='header item'>
+          <button className='ui button'>Dodaj</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
 
 
-// function ContactList() {
-//   return(
-//     <ul className='ui relaxed divided list selection'>
-//       <ContactItem
-//         login='typeofweb1'
-//         name='Lena'
-//         department='JavaScript Developer'
-//       />
-//       <ContactItem
-//         login='typeofweb2'
-//         name='Brian'
-//         department='Human Resources'
-//       />
-//       <ContactItem
-//         login='typeofweb3'
-//         name='Rick'
-//         department='QA'
-//       />
-//     </ul>
-//   );
-// }
+function ContactList() {
+  return(
+    <ul className='ui relaxed divided list selection'>
+      <ContactItem
+        login='typeofweb1'
+        name='Lena'
+        department='JavaScript Developer'
+      />
+      <ContactItem
+        login='typeofweb2'
+        name='Brian'
+        department='Human Resources'
+      />
+      <ContactItem
+        login='typeofweb3'
+        name='Rick'
+        department='QA'
+      />
+    </ul>
+  );
+}
 
 
-// function ContactItem({login, name, department}) {
-//   return (
-//     <li className='item'>
-//       <UserAvatar login={login}/>
-//       < div className='content'>
-//         <h4 className='header'>{name}</h4>
-//         <div className='description'>{department}</div>
-//       </div>
-//     </li>
-//   );
-// }
+function ContactItem({login, name, department}) {
+  return (
+    <li className='item'>
+      <UserAvatar login={login}/>
+      < div className='content'>
+        <h4 className='header'>{name}</h4>
+        <div className='description'>{department}</div>
+      </div>
+    </li>
+  );
+}
 
 
-// function UserAvatar({login}) {
-//   let avatar;
+function UserAvatar({login}) {
+  let avatar;
 
-//   if(login.includes('@'))
-//   {
-//     avatar='https://api.hello-avatar.com/adorables/55/' + {login} + '.png'; 
-//   }
-//   else
-//   {
-//     avatar='https://www.gravatar.com/avatar/sun';
-//   }
+  if(login.includes('@'))
+  {
+    avatar='https://api.hello-avatar.com/adorables/55/' + {login} + '.png'; 
+  }
+  else
+  {
+    avatar='https://www.gravatar.com/avatar/sun';
+  }
     
-//   return (
-//     <img src={avatar} className='ui mini rounded image' />
-//   )
-// }
+  return (
+    <img src={avatar} className='ui mini rounded image' />
+  )
+}
 
 
-// function App() { 
-//   return (
-//       <div>
-//         <AppHeader />
-//         <main className='ui main text container'>
-//           <ContactList />
-//         </main>
-//       </div>
-//     );
-//   }
+function App() { 
+  return (
+      <div>
+        <AppHeader />
+        <main className='ui main text container'>
+          <ContactList />
+        </main>
+      </div>
+    );
+  }
 
-// export default App;
+export default App;
 
 
 
@@ -250,3 +250,39 @@ function App() {
 
 
 
+
+
+
+// State counter
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {counter: 0};
+  }
+  
+  // These two functions bellow dosn't see 'this' that's why have to be bind.
+  increment() { 
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrement() { 
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment.bind(this)}>+</button>
+        <output>{this.state.counter}</output>
+        <button onClick={this.decrement.bind(this)}>-</button>
+      </div>
+    );
+  }
+}
+
+export default App;
